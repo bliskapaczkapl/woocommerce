4a2baa2c77d320244c7de7901a9e8fe5e5d039da
Paweł Karbowniczek <pkarbowniczek@divante.pl>
2019-11-29 16:02:22 +0100

    Fixed problem with cod checkbox

--- wp-content/plugins/bliskapaczka-shipping-method/class-bliskapaczka-shipping-method.php
+++ wp-content/plugins/bliskapaczka-shipping-method/class-bliskapaczka-shipping-method.php
@@ -109,10 +109,10 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		if ( 'bliskapaczka' === $method->id && is_checkout() === true ) {
 			$payment_method = WC()->session->get( 'chosen_payment_method' );
 			$operators      = $helper->getOperatorsForWidget();
-			$codOnly = 'false';
+			$cod_only       = 'false';
 			if ( 'cod' === $payment_method ) {
 				$operators = $helper->recalculatePrice( $operators );
-				$codOnly = 'true';
+				$cod_only  = 'true';
 			}
 			// @codingStandardsIgnoreStart
 			echo " <a href='#bpWidget_wrapper' " .
@@ -123,7 +123,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 					'", ' .
 					esc_html( ( 'test' === $helper->getApiMode( $bliskapaczka->settings['BLISKAPACZKA_TEST_MODE'] ) ? 'true' : 'false' ) ) .
                     ',' .
-                    esc_html( $codOnly) .
+                    esc_html( $cod_only) .
 					")'>" .
 					esc_html( 'Wybierz punkt dostawy' ) . '</a>';
 			// @codingStandardsIgnoreEnd
