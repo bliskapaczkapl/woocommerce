f5c5de762e1bbfa1e0eab9f49058184ac439f63d
markoWar <4921750+markoWar@users.noreply.github.com>
2020-06-29 13:38:33 +0200

    Base exception for all exceptions of Bliskapaczka plugin

---
+++ wp-content/plugins/bliskapaczka-shipping-method/includes/class-bliskapaczka-exception.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Bliskapaczka.pl WooCommerce plugin
+ */
+
+/**
+ * Base exception for all exceptions of Bliskapaczka plugin
+ *
+ */
+class Bliskapaczka_Exception extends \Exception
+{
+}
