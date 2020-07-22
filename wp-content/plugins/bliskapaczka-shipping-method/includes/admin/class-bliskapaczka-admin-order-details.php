26df11ed19ffd2fef369c76774b6b89f87464850
Ireneusz Tokarski <100idei@users.noreply.github.com>
2020-07-22 08:23:06 +0200

    Moved function.

--- wp-content/plugins/bliskapaczka-shipping-method/includes/admin/class-bliskapaczka-admin-order-details.php
+++ wp-content/plugins/bliskapaczka-shipping-method/includes/admin/class-bliskapaczka-admin-order-details.php
@@ -74,24 +74,4 @@ class Bliskapaczka_Admin_Order_Details {
 	}
 
 }
-/**
- * Detailed info about point to point delivery
- *
- * @param array $formatted_meta Formatted order metadata.
- */
-function bliskapaczka_order_meta_data_view( $formatted_meta ) {
-	foreach ( $formatted_meta as $obj ) {
-		if ( '_bliskapaczka_posCode' === $obj->display_key ) {
-			$obj->display_key = esc_html( __( 'Point code', 'bliskapaczka-shipping-method' ) );
-		}
-		if ( '_bliskapaczka_posOperator' === $obj->display_key ) {
-			$obj->display_key = esc_html( __( 'Operator', 'bliskapaczka-shipping-method' ) );
-		}
-		if ( '_bliskapaczka_posInfo' === $obj->display_key ) {
-			$obj->display_key = esc_html( __( 'Point info', 'bliskapaczka-shipping-method' ) );
-		}
-	}
 
-	return $formatted_meta;
-}
-add_filter( 'woocommerce_order_item_get_formatted_meta_data', 'bliskapaczka_order_meta_data_view', 10, 2 );
